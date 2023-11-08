a2013021f1fcf9f1fae357acb6b12124cec696f6
Bogdan Boicu <bogdan.boicu16@gmail.com>
2023-11-08 14:33:01 +0200

    #3 Created CoreConcepts and TabButton components

---
+++ src/components/CoreConcepts/CoreConcepts.jsx
@@ -0,0 +1,10 @@
+import './CoreConcepts.css'
+export function CoreConcepts(props) {
+    return (
+        <li>
+        <img src={props.image} alt={props.title} />
+        <h3>{props.title}</h3>
+        <p>{props.description}</p>
+      </li>
+    );
+  }
